a9cd97efd345a7d104a871d26fb703abf22fd32c
KristopherJnr <chrisebosiethomas@gmail.com>
2022-09-08 21:54:55 -0700

    git pushing files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+ble File  12 lines (11 sloc)  178 Bytes
+
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
